1bd295e51aa29a3520e3c2b26d938ccbd2cdc34f
Швецов Артем Геннадьевич <a.shvecov@RGS.LOCAL>
2023-01-19 18:07:04 +0900

    add db file

--- pkg/db.go
+++ pkg/db.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
+	"simpleProject/pkg/model"
 )
 
-type db map[DocumentManagment]string
-
-func main() {
-	data := map[DocumentManagment]string{
+var Data = map[int]model.DocumentManagment{
+	1: {
 		Id:          "1",
 		Name:        "Оказание услуг по передаче данных по широкополосному каналу Интернет",
 		Date:        "28.12.2022",
@@ -18,6 +17,22 @@ func main() {
 		Distributor: "пао ростелеком",
 		Metod:       "аукцион",
 		Sum:         "150 000",
-	}
-	fmt.Println(data)
+	},
+	2: {
+		Id:          "2",
+		Name:        "Оказание услуг по передаче данных по широкополосному каналу Интернет",
+		Date:        "28.12.2022",
+		Description: "Описание",
+		File:        "generic UUID file",
+		DateIn:      "01.01.2023",
+		DateOut:     "31.12.2023",
+		Distributor: "пао ростелеком",
+		Metod:       "аукцион",
+		Sum:         "150 000",
+	},
+}
+
+func main() {
+
+	fmt.Println(Data)
 }
