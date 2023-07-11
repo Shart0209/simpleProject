cac358e52f5cce314c6e78b3df9fa621cc54901b
Артём <shart0209@gmail.com>
2023-07-11 18:17:55 +0900

    Update test_values.sql

--- migrations/test_values.sql
+++ migrations/test_values.sql
@@ -7,19 +7,19 @@ INSERT INTO authors (login, name, role, pswd_hash, email)
 VALUES ('test', 'test', 'user', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','example@gmail.com');
 
 
-WITH new_contract as  (
-	INSERT INTO contracts (title, numb, price, date, start_date, end_date, description, files)
-	VALUES (
-		'Оказание услуг Интернет','ЧИТ-13-45', '152000', '2023-07-01', '2023-07-01','2023-12-31', 'test test description', '[
-  {
-    "id": "e64b7418-1478-4e78-a866-74e0c4b77419-0.txt",
-    "name": "гос контракт тест.txt",
-    "path": "/upload/ГК № ЧИТ-13-45",
-    "size": 30936
-  }
-]'
-	)
-	RETURNING contract_id 
-	)
-	INSERT INTO commons
-	SELECT contract_id, 2, 17, 1, 1, true, '2023-07-01', '2023-07-01' from new_contract
+-- WITH new_contract as  (
+-- 	INSERT INTO contracts (title, numb, price, date, start_date, end_date, description, files)
+-- 	VALUES (
+-- 		'Оказание услуг Интернет','ЧИТ-13-45', '152000', '2023-07-01', '2023-07-01','2023-12-31', 'test test description', '[
+--   {
+--     "id": "e64b7418-1478-4e78-a866-74e0c4b77419-0.txt",
+--     "name": "гос контракт тест.txt",
+--     "path": "/upload/ГК № ЧИТ-13-45",
+--     "size": 30936
+--   }
+-- ]'
+-- 	)
+-- 	RETURNING contract_id 
+-- 	)
+-- 	INSERT INTO commons
+-- 	SELECT contract_id, 2, 17, 1, 1, true, '2023-07-01', '2023-07-01' from new_contract
